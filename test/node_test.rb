9479625f6d99b411db22dba15ba93f5063823ec4
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-09 13:34:17 -0600

    right_node_set

--- test/node_test.rb
+++ test/node_test.rb
@@ -23,4 +23,21 @@ class NodeTest < Minitest::Test
     assert_equal 0, node.depth
   end
 
+  def test_left_node_set_returns_depth
+    node_1 = Node.new(50, 'Gold')
+
+
+    assert_equal 1, node_1.left_node_set(49, 'Spiderman')
+    assert_equal 2, node_1.left_node_set(25, 'Fargo')
+    assert_equal 3, node_1.left_node_set(12, 'Batman')
+  end
+
+  def test_right_node_set_returns_depth
+    node_1 = Node.new(50, 'Gold')
+
+
+    assert_equal 1, node_1.right_node_set(58, 'Spiderman')
+    assert_equal 2, node_1.right_node_set(60, 'Fargo')
+    assert_equal 3, node_1.right_node_set(80, 'Batman')
+  end
 end
