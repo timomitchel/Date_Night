b2ea3e6264e2d28d54cc154d15f38b6c54f9ea2e
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-09 13:25:45 -0600

    node tests passing for initialize

--- test/node_test.rb
+++ test/node_test.rb
@@ -6,10 +6,21 @@ require "./lib/binary_search_tree"
 
 class NodeTest < Minitest::Test
 
-  def test_node_class_exists
-    node = Node.new(99, 'Cruel Intentions')
+  attr_reader :node
+  def setup
+    @node = Node.new(99, 'Cruel Intentions')
+  end
 
+  def test_node_class_exists
     assert_instance_of Node, node
+  end
+
+  def test_initialize_variables
+    assert_equal 99, node.score
+    assert_equal 'Cruel Intentions', node.title
+    assert_nil nil, node.left
+    assert_nil nil, node.right
+    assert_equal 0, node.depth
   end
 
 end
